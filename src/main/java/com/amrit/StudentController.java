99fac5efb26b6101c5d9bb53900ed4e19946710f
amrituuu <amritj2510@gmail.com>
2026-04-17 18:02:29 +0530

    Fix CORS for deployed frontend

--- src/main/java/com/amrit/StudentController.java
+++ src/main/java/com/amrit/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     @Autowired
